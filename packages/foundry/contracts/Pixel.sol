7a194bdc7f280b2adb53da4f9a421cc119a8697b
Cypher <decypher@skiff.com>
2023-11-18 23:06:31 +0300

    vouch addresses

--- packages/foundry/contracts/Pixel.sol
+++ packages/foundry/contracts/Pixel.sol
@@ -17,6 +17,16 @@ contract Pixel  {
     mapping (address => bytes32[]) public vouchesSent;
 
 
+    function DOMAIN_HASH() public view returns (bytes32) {
+        return keccak256(abi.encode(block.chainid, address(this)));
+    }
+
+    /// @notice Generates hash 
+    function generateVouchHash(address sender, address recipient) public view returns (bytes32 vouchId) {
+        vouchId = keccak256(abi.encode(DOMAIN_HASH(), sender, recipient));
+    }
+
+
     function register( string memory userName ) external {
         // require(vouchesReceived[msg.sender].length > 0, "Need one vouch to register");
         require(keccak256(abi.encodePacked(userNames[msg.sender])) == keccak256(abi.encodePacked("")), "Cannot register twice");
@@ -37,5 +47,55 @@ contract Pixel  {
         }
         coldWallets[msg.sender] = coldWallet;
     }
+
+
+    // Vouching
+
+
+    function vouch(address recipient) public payable {
+        if (msg.value > 0) {
+            // could keep track of new signups for referral program
+            // require(msg.value >= minBalance, "Not enough ether for new viber");
+            recipient.call{value: msg.value}("");
+        }        
+        bytes32 vouchId = generateVouchHash(msg.sender, recipient);
+        vouchesSent[msg.sender].push(vouchId);
+        vouchesReceived[recipient].push(vouchId);
+        _vouch(msg.sender, vouchId);
+    }
+
+    function _vouch(address sender, bytes32 vouchId) internal {
+        require(vouchSender[vouchId] == address(0), "Vouch already exists");
+        vouches.push(vouchId);
+        vouchSender[vouchId] = sender;
+    }
+
+    function vouchReceivedCount(address user) public view returns (uint256) {
+        return vouchesReceived[user].length;
+    }
+
+    function vouchSentCount(address user) public view returns (uint256) {
+        return vouchesSent[user].length;
+    }
+
+
+    // Trust
+    function getTrustScore(address user, address vouched) public view returns (uint256) {
+        uint256 memberLength = vouchesSent[user].length;
+        if (memberLength == 0) return 0;
+        uint256 score = 0;
+        bytes32 vouchId;
+        address vouchMember;
+
+        for (uint256 i = 0; i < memberLength; i++) {
+            vouchMember = vouchesSent[user][i];
+            vouchId = generateVouchHash(vouchMember, vouched);
+            if ( vouchSender[vouchId] == vouchMember ) {
+                score++;
+            }
+        }
+        return score;
+    }
+
 
 }
